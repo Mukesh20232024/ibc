ff142c11b44a5c8fd88d228c209d9e404f3a1ec9
Mukesh20232024 <mvmv1727@gmail.com>
2023-11-03 19:57:42 +0530

    latest

--- src/components/ForgatPwd.jsx
+++ src/components/ForgatPwd.jsx
@@ -23,7 +23,7 @@ export default function ForgatPwd() {
                     <p className='w-96 text-left'>Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.</p>
                     <form onSubmit={handleSubmit} className='mt-8'>
                         <div className="mb-4">
-                            <input onChange={(e) => setEmail(e.target.value)} value={email} type="password" id="password" placeholder='Enter Email Address' className="w-full px-20 py-2 border border-gray-300 rounded" required />
+                            <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" id="password" placeholder='Enter Email Address' className="w-full px-20 py-2 border border-gray-300 rounded" required />
                         </div>
                         <button type="submit" className="w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">Email Password Reset Link</button>
                     </form>
